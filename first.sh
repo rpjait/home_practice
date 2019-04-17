1084d744ae8d33809513ad6936d57b62fb1659be
ram j <jaiswal.ramprakash1@gmail.com>
2019-04-17 22:58:58 +0530

    tor

--- first.sh
+++ first.sh
@@ -1,9 +1,7 @@
 #/bin/bash
-echo "practiceing at home"
+
 echo "launching tor browser"
-cd ../
-cd /Downloads/tor-browser_en-US/
+cd /home/ram/Downloads/tor-browser_en-US
 ./start-tor-browser.desktop
-wait 60
 exit
 
